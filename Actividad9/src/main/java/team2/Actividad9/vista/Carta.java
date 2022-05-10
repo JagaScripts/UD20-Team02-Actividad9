b52b73b42addd7e0f42d9f79440a2eb4dc1e0ca8
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-05-10 19:15:10 +0200

    Construyendo botonCarta

--- Actividad9/src/main/java/team2/Actividad9/vista/Carta.java
+++ Actividad9/src/main/java/team2/Actividad9/vista/Carta.java
@@ -3,7 +3,12 @@
  */
 package team2.Actividad9.vista;
 
+import java.awt.Dimension;
+
 import javax.swing.JToggleButton;
+
+import es.seas.unidad3.extras.parejas.model.entities.Card;
+import es.seas.unidad3.extras.parejas.view.components.CardButtonListener;
 
 /**
  * Actividad9 - team2.Actividad9.vista - Carta
@@ -14,6 +19,22 @@ import javax.swing.JToggleButton;
  * 
  * Fecha de creación 10/05/2022
  */
-public class Carta extends JToggleButton {
+public class BotonCarta extends JToggleButton {
 
+	/**
+     * Constructor por defecto que prepara el componente con las propiedades
+     * configuradas como necesitamos y que admite una carta como parámetro.
+     *
+     * @param card Card.
+     */
+    public BotonCarta() {
+    	Dimension dimension = new Dimension(140, 140);
+        this.setPreferredSize(dimension);
+        this.setDisabledIcon(new javax.swing.ImageIcon(getClass().getResource(
+                Card.BACK_IMAGE)));
+        this.setIcon(new javax.swing.ImageIcon(getClass().getResource(
+                card.getImage())));
+        this.addMouseListener(new CardButtonListener());
+        this.setEnabled(card.isGuessed());
+    }
 }
