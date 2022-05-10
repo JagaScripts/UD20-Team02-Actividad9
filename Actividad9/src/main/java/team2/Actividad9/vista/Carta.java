8d50c4dddb4093ed2650bfc8042ad24051accf79
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-05-10 19:11:13 +0200

    Creada la clase BotonCarta

---
+++ Actividad9/src/main/java/team2/Actividad9/vista/Carta.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package team2.Actividad9.vista;
+
+import javax.swing.JToggleButton;
+
+/**
+ * Actividad9 - team2.Actividad9.vista - Carta
+ *
+ * @author Daniel Fernández Cacho
+ * @author Joan Hurtado García
+ * @author Jose Antonio González Alcántara
+ * 
+ * Fecha de creación 10/05/2022
+ */
+public class Carta extends JToggleButton {
+
+}
